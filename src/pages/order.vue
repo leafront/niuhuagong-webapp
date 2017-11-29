a3f37bd8c05be4abd1b32eec2193f0e2ea0fc183
叶亮 <yeliang@83chedai.com>
2017-11-29 17:57:34 +0800

    添加订单页面

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -8,17 +8,438 @@
 				<span>我的订单</span>
 			</div>
 			<ul class="order_menu_list">
-				<li class="active"><span>全部</span></li>
-				<li><span>待支付</span></li>
-				<li><span>待发货</span></li>
-				<li><span>待收货</span></li>
-				<li><span>待评价</span></li>
+				<li v-for="(item,i) in orderTxt" :class="{'active': index == item.status}" @click="showTab(i)"><span>{{item.name}}</span></li>
 			</ul>
 		</div>
+		<!--全部-->
+		<template v-if="list && list.length">
+			<div class="order_tab">
+				<div class="order_item">
+					<div class="order_item_tit">
+						<span>订单号：561315641266600035</span>
+						<strong>待发货</strong>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_money">
+						<span>运费:¥1008.2+¥100.00(送货上门)</span>
+						<div class="order_money_total">
+							<strong>合计:</strong>
+							<i>￥3853.2</i>
+						</div>
+					</div>
+					<div class="order_status">
+						<button class="order_btn_status">提醒发货 </button>
+					</div>
+				</div>
+				<div class="order_item">
+					<div class="order_item_tit">
+						<span>订单号：561315641266600035</span>
+						<strong>待发货</strong>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_money">
+						<span>运费：¥300</span>
+						<div class="order_money_total">
+							<strong>合计:</strong>
+							<i>￥3853.2</i>
+						</div>
+					</div>
+					<div class="order_status">
+						<button class="order_btn_status">取消订单</button>
+						<button class="order_btn_status">我要支付</button>
+					</div>
+				</div>
+				<div class="order_item">
+					<div class="order_item_tit">
+						<span>订单号：561315641266600035</span>
+						<strong class="order_item_status">已取消</strong>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_money">
+						<span>运费:¥1008.2+¥100.00(送货上门)</span>
+						<div class="order_money_total">
+							<strong>合计:</strong>
+							<i>￥3853.2</i>
+						</div>
+					</div>
+					<div class="order_status">
+						<button class="order_btn_status">删除订单</button>
+					</div>
+				</div>
+				<div class="order_item">
+					<div class="order_item_tit">
+						<span>订单号：561315641266600035</span>
+						<strong>已发货</strong>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_money">
+						<span>运费:¥1008.2+¥100.00(送货上门)</span>
+						<div class="order_money_total">
+							<strong>合计:</strong>
+							<i>￥3853.2</i>
+						</div>
+					</div>
+					<div class="order_status">
+						<button class="order_btn_status">查看物流</button>
+						<button class="order_btn_status">确认收货</button>
+					</div>
+				</div>
+				<div class="order_item">
+					<div class="order_item_tit">
+						<span>订单号：561315641266600035</span>
+						<strong>待评价</strong>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_money">
+						<span>运费:¥1008.2+¥100.00(送货上门)</span>
+						<div class="order_money_total">
+							<strong>合计:</strong>
+							<i>￥3853.2</i>
+						</div>
+					</div>
+					<div class="order_status">
+						<button class="order_btn_status">我要评价</button>
+					</div>
+				</div>
+				<div class="order_item">
+					<div class="order_item_tit">
+						<span>订单号：561315641266600035</span>
+						<strong>已评价</strong>
+					</div>
+					<div class="order_info">
+						<img src="//img.alicdn.com/imgextra/i3/17413633/TB225tKecjI8KJjSsppXXXbyVXa_!!0-saturn_solar.jpg_210x210.jpg"/>
+						<div class="order_info_txt">
+							<p>苏州秧浦水性色浆YP系列</p>
+							<span>YP502大红</span>
+							<span>20公斤/桶×１（小计:20公斤）</span>
+							<strong>￥185</strong>
+						</div>
+					</div>
+					<div class="order_money">
+						<span>运费:¥1008.2+¥100.00(送货上门)</span>
+						<div class="order_money_total">
+							<strong>合计:</strong>
+							<i>￥3853.2</i>
+						</div>
+					</div>
+					<div class="order_status">
+						<button class="order_btn_status">再次购买</button>
+					</div>
+				</div>
+			</div>
+		<!--全部-->
+		</template>
+		<template v-else>
+			<div class="order_empty">
+				<img src="./images/order_empty_bg.png"/>
+				<p>还没有购买商品</p>
+			</div>
+		</template>
 	</div>
 </template>
 
+<script>
+
+	import AppFooter from '@/components/common/footer'
+	
+	export default {
+		
+		components: {
+
+			AppFooter
+			
+		},
+		
+		data () {
+			
+			return {
+				
+				list:[undefined],
+				
+				index: 0,
+				
+				orderTxt: [{
+					name:'全部',
+					status: 0
+				},{
+					name:'待支付',
+					status: 1
+				},{
+					name:'待发货',
+					status: 2
+				},{
+					name:'待收货',
+					status: 3
+				},{
+					name:'待评价',
+					status: 4
+				}]
+				
+			}
+			
+		},
+		
+		methods: {
+
+			showTab (index) {
+				
+				this.index = index;
+				
+			}
+		}
+		
+	}
+	
+</script>
+
 <style lang="scss">
+	
+	
+	.order_empty{
+		
+		padding-top: 40%;
+		
+		display:flex;
+		
+		align-items: center;
+		
+		justify-content: center;
+		
+		flex-direction: column;
+		
+		p{
+			
+			margin-top: .3rem;
+			
+			font-size: .28rem;
+		}
+		
+		img{
+			
+			width: 1.96rem;
+			
+			height: 1.3rem;
+			
+		}
+		
+	}
+	
+	
+	.order_status{
+		
+		height: .75rem;
+		
+		display: flex;
+		
+		align-items: center;
+		
+		justify-content: flex-end;
+		
+		padding-right: .26rem;
+		
+		.order_btn_status{
+			
+			margin-left: .24rem;
+		
+			height: .46rem;
+			
+			line-height: .46rem;
+			
+			padding: 0 .18rem;
+			
+			border-radius: .44rem;
+			
+			color: #252525;
+			
+			background: #fff;
+			
+			border:1px solid #a8a8a8;
+		
+		}
+		
+	}
+	
+	.order_money{
+		
+		display:flex;
+		
+		align-items: center;
+		
+		justify-content: space-between;
+		
+		height: .75rem;
+		
+		padding: 0 .3rem;
+		
+		border-bottom: 1px solid #f1f1f1;
+		
+		span{
+			
+			color: #252525;
+			
+		}
+		
+	}
+	
+	.order_money_total{
+		
+		strong{
+			
+			color: #252525;
+		}
+		
+		span{
+			
+			color: #252525;
+			
+		}
+		
+		i{
+			
+			color: #fe8900;
+			
+			font-size: .28rem;
+			
+			padding-left: .2rem;
+			
+		}
+		
+	}
+	
+	.order_info{
+		
+		padding: .4rem .3rem;
+		
+		display: flex;
+		
+		align-items: center;
+		
+		border-bottom: 1px solid #f1f1f1;
+		
+		img{
+		
+			width: 1.5rem;
+			
+			height: 1.5rem;
+			
+			vertical-align: top;
+			
+		}
+		
+	}
+	
+	.order_info_txt{
+		
+		padding-left: .28rem;
+		
+		p{
+			color: #252525;
+			
+			padding-bottom: .15rem;
+		}
+		
+		span{
+			
+			display:block;
+			
+		}
+		
+		strong{
+			
+			padding-top: .15rem;
+			
+			display:block;
+		
+			color: #252525;
+			
+		}
+		
+	}
+	
+	.order_item{
+		
+		background: #fff;
+		
+		margin-top: .24rem;
+		
+	}
+	
+	.order_item_tit{
+		
+		display:flex;
+		
+		align-items: center;
+		
+		justify-content: space-between;
+		
+		padding: 0 .3rem;
+	
+		height: .75rem;
+		
+		border-bottom: 1px solid #f5f5f5;
+		
+		strong{
+			
+			color:#008aec;
+			
+			&.order_item_status{
+				
+				color: #a2a2a2;
+				
+			}
+			
+		}
+	
+	}
 	
 	.order_menu{
 		
